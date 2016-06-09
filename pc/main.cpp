b270944447f36f7e613756ffbed746c78d14e1c8
冯铭扬 <fengmingyang@fengmingyangdemacbook-air.local>
2016-06-09 21:31:54 +0800

    Initial Commit

---
+++ pc/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pc
+//
+//  Created by 冯铭扬 on 16/6/9.
+//  Copyright © 2016年 冯铭扬. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
